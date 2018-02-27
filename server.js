d155fa8755c0e732d201d7f667d2f196476515d8
Kelsi Summers <kelsisummers@Kelsis-MacBook-Air.local>
2018-02-26 21:18:58 -0700

    alert

--- server.js
+++ server.js
@@ -112,20 +112,20 @@ app.get("/api/waitlist", function(req, res) {
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   
     console.log('this is our res or waitlist', newReso);
-    const blob = {
-      reserved: false,
-      message: ""
-    }
+
     if (reservations.length < 5) {
       reservations.push(newReso);
-      blob.reserved = true;
-      blob.message = " Hoorah"
+      res.json({
+        reserved: true
+      });
     } else {
       waitList.push(newReso);
-      blob.message = "booo"
+      res.json({
+        reserved: false
+      })
     }
   
-    res.json(blob);
+    
 
 
 
